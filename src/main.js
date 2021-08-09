23ef11afbdd3503c0250b723a935c6d2185476d7
Maksim Morozov <maxmorozov.art@gmail.com>
2021-08-09 10:25:11 +0300

    Add basic prettier config

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
-'use strict'
+const username = 'max';
 
-const username = 'max'
+typeof username === 'string';
 
-typeof username === 'strng'
-
-if (!'serviceWorker' in navigator) {
-
+if (!('serviceWorker' in navigator)) {
+  // some comment
 }
 
-const greeting = 'hello'
-console.log('${greeting} world!')
-[1, 2, 3].forEach(x => console.log(x))
+const greeting = 'hello';
+console.log(`${greeting} world!`);
+[1, 2, 3].forEach(x => console.log(x));
